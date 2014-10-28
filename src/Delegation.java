4f02e04b47172171bc3456278bcb6327ddf286ce
saran1191 <Saravanan@Saravanan>
2014-10-27 20:27:04 -0400

    * Delegation simulated - Not sure if its correct. Have to check with TA. *

--- src/Delegation.java
+++ src/Delegation.java
@@ -75,34 +75,181 @@ class C112 extends C11 {
 // -------SIMULATING CLASS INHERITANCE BY DELEGATION ---------
 
 interface I1 {
-	 
+	public int m1();
+	public int p1(int m);
+	public int q1(int m);
+	public int getVariable();
 }
 
 interface I11 extends I1 {
-	 
+	public int m11();
 }
 
 interface I111 extends I11 {
-	 
+	public int m111();
 }
 
 interface I112 extends I11 {
-	 
+	public int m112();
 }
 
 class D1 implements I1 {
-	 
+	int a1 = 1;
+	I11 subclassInstance = null;
+	
+	public D1() {}
+	
+	public D1(I11 instance) {
+		subclassInstance = instance;
+	}
+
+	@Override
+	public int m1() {
+		int p1 = subclassInstance == null ? p1(100) : subclassInstance.p1(100);
+		int q1 = subclassInstance == null ? q1(100) : subclassInstance.q1(100);
+		return a1 + p1 + q1;
+	}
+
+	@Override
+	public int p1(int m) {
+		return m;
+	}
+	
+	@Override
+	public int q1(int m) {
+		return m;
+	}
+
+	@Override
+	public int getVariable() {
+		return a1;
+	}
 }
 
 class D11 implements I11 {
-	 
+
+	int a11 = 11;
+	I1 parentInstance = null;
+	I11 subclassInstance = null;
+	
+	public D11() {
+		parentInstance = new D1();
+	}
+	
+	public D11(I11 instance) {
+		subclassInstance = instance;
+		parentInstance = new D1(this);
+	}
+	
+	@Override
+	public int m1() {
+		return parentInstance.m1();
+	}
+	
+	@Override
+	public int m11() {
+		return m1() + q1(200);
+	}
+
+	@Override
+	public int p1(int m) {
+		if(subclassInstance != null) {
+			return subclassInstance.p1(m);
+		} else {  
+			return m * parentInstance.getVariable();
+		
+		}
+	}
+
+	@Override
+	public int q1(int m) {
+		return m + a11;
+	}
+
+	@Override
+	public int getVariable() {
+		return a11;
+	}
 }
 
  
 class D111 implements I111 { 
+	int a111 = 111;
+	I1 rootParentInstance = null;
+	I11 parentInstance = null;
+	
+	public D111() {
+		rootParentInstance = new D1();
+		parentInstance = new D11(this);
+	}
 
+	@Override
+	public int m1() {
+		return parentInstance.m1();
+	}
+	
+	@Override
+	public int m11() {
+		return parentInstance.m11();
+	}
+	
+	@Override
+	public int m111() {
+		return m1() + m11() + a111;
+	}
+	
+	@Override
+	public int p1(int m) {
+		return m * rootParentInstance.getVariable() * parentInstance.getVariable();
+	}
+
+	@Override
+	public int q1(int m) {
+		return parentInstance.q1(m);
+	}
+
+	@Override
+	public int getVariable() {
+		return a111;
+	}
 }
 
 class D112 implements I112 {
-	 
+	int a112 = 112;
+	I1 rootParentInstance = null;
+	I11 parentInstance = null;
+	
+	public D112() {
+		rootParentInstance = new D1();
+		parentInstance = new D11(this);
+	}
+
+	public int m112() {
+		return m1() + m11() + a112;
+	}
+
+	@Override
+	public int p1(int m) {
+		return m * rootParentInstance.getVariable() * parentInstance.getVariable() * a112;
+	}
+
+	@Override
+	public int m11() {
+		return parentInstance.m11();
+	}
+
+	@Override
+	public int m1() {
+		return parentInstance.m1();
+	}
+
+	@Override
+	public int q1(int m) {
+		return parentInstance.q1(m);
+	}
+
+	@Override
+	public int getVariable() {
+		return a112;
+	}
 }
