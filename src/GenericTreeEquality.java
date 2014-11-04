fef0a903dea285f018e9009a0b4e9d6907e8b589
nramamur <nramamur@buffalo.edu>
2014-11-04 10:04:21 -0500

    Generic Tree Equality - Implemented.

--- src/GenericTreeEquality.java
+++ src/GenericTreeEquality.java
@@ -30,35 +30,69 @@ class Tree<T extends Comparable<T>> {
 }
 
 class Iter<T extends Comparable<T>> {
+	private Stack<Tree<T>> nodeStack;
+	public Iter() {
+		nodeStack = new Stack<Tree<T>>();
+	}
 
-// define here the external iterator operations, done and next
+	private void traverseLeft(Tree<T> tree){
+		for (Tree<T> current = tree; current != null; current = current.left) {
+			nodeStack.push(current);
+		}
+	}
 
+	public boolean done() {	
+		return nodeStack.isEmpty();
+	}
+
+	public T next(Tree<T> tree) {
+		if (nodeStack.isEmpty()){
+			traverseLeft(tree);
+		}
+		Tree<T> current = nodeStack.pop();
+		for (Tree<T> child = current.right; child != null; child = child.left) {
+			nodeStack.push(child);
+		}
+		return current.value;
+	}
 }
 
 public class GenericTreeEquality {
-
-     static <T extends Comparable<T>> boolean equals(Tree<T> tree1, Tree<T> tree2) {
-
-	// define here the equality test
-
-	}
-	
-	public static void main(String[] args) {
+	static boolean equalityCheck = true;
+	static <T extends Comparable<T>> boolean equals(Tree<T> tree1, Tree<T> tree2) {
+		Iter<T> iteratorOne = new Iter<T>();
+		Iter<T> iteratorTwo = new Iter<T>();
+		T nextOne = null;
+		T nextTwo = null;
+		do {
+			nextOne = iteratorOne.next(tree1);
+			nextTwo = iteratorTwo.next(tree2);			
+			if (!(nextOne.compareTo(nextTwo) == 0)){
+				equalityCheck = false;
+				break;
+			}
+		} while (!iteratorOne.done() && !iteratorTwo.done());
 		
+		// Clean-up : Return false when traversal is complete for ONLY one tree
+		boolean firstTreeDone = iteratorOne.done() && !iteratorTwo.done();
+		boolean secondTreeDone = !iteratorOne.done() && iteratorTwo.done();
+		if (firstTreeDone || secondTreeDone)
+			equalityCheck = false;
+		return equalityCheck;
+	}
+
+	public static void main(String[] args) {
 		Tree<Integer> tree1 = new Tree<Integer>(50);
 		tree1.insert(70);
 		tree1.insert(80);	
 		tree1.insert(90);
 		tree1.insert(100);
-		
 		Tree<Integer> tree2 = new Tree<Integer>(100);
 		tree2.insert(90);
 		tree2.insert(80);	
 		tree2.insert(70);
 		tree2.insert(50);
-		
-		System.out.println(equals(tree1, tree2 ));
+		System.out.println(equals(tree1, tree2));
 	}
 }
-    
- 
+
